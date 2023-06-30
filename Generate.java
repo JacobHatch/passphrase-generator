80b8f91d8808e960f61cbc4c5565517bcff5b5cf
Jacob Hatch <129548421+JacobHatch@users.noreply.github.com>
2023-06-30 13:10:56 -0400

    added comments/cleaned up a little
    still need to proofread

--- Generate.java
+++ Generate.java
@@ -9,13 +9,15 @@ import java.util.Random;
 class Generate{
 	public static void main(String[] args){
 
+			//Initialization of scanner object to be used for obtaining user input
 			Scanner userInput = new Scanner(System.in);
 
+			//These three lines are used to get the choice of whether to create a familiar passphrase or randomly generate password
 			System.out.println("\nWould you like to create a familiar passphrase or a randomly generated password?");
 			System.out.println("(Type 'phrase' or 'word' to choose and press enter.)\n");
-			
 			String ans = userInput.nextLine();
-			
+
+			//Check that the input is valid
 			if((!ans.equals("phrase")) && (!ans.equals("word"))){
 
 				System.out.print("Correct usage is typing <word> or <phrase>, exiting program");
@@ -23,13 +25,14 @@ class Generate{
 		
 			}
 
+			//If creating a familiar passphrase is chosen:
+			//    Get the components of the passphrase as user input
+			//    Secure the user inputs and add them to the passphrase
+			//    Print the new secure passphrase
 			if(ans.equals("phrase")){
 
 				String s1, s2, i1, p1;
 				String passphrase = "";
-				Random rand = new Random();
-				long seed = rand.nextLong();
-				rand.setSeed(seed);
 				
 				System.out.println("\nYou have chosen to create a personalized passphrase.");
 				System.out.println("To create a passphrase, you will use memorable information.");
@@ -51,6 +54,11 @@ class Generate{
 				System.out.println("\nYour passphrase is: " + passphrase + "\n");	
 			}
 
+			//If creating a generated password is chosen:
+			//    Initialize the random number, random seed, password length, and password string
+			//    Recommend a proper password length and get the length as user input
+			//    Create a password of that length using the ASCII table, specifically characters available for user input
+			//    Print the new password
 			if(ans.equals("word")){
 				
 				Random rand = new Random();
@@ -64,6 +72,7 @@ class Generate{
 				System.out.println("(it is recommended at leat 10 characters in length)\n");
 				passLength = userInput.nextInt();
 
+				// This loop loops for the password length and randomly selects a keyboard inputtable ASCII table characer, adding it to the password
 				for(int i = 0; i < passLength; i++){
 
 					int randNum = rand.nextInt(95)+32;
@@ -74,11 +83,21 @@ class Generate{
 			}
 	}
 
+	//This function takes a word or phrase without spaces as a parameter and returns a new, similar version of that word or phrase that has letters replaced with capital letters, numbers, or symbols.
+	//Special note on the fact that these replacement characters are chosen to still look similar to the original word or phrase to keep its familiarity.
+	//Requires: The input of a String. 
+	//Assignable: NONE
+	//Ensures: The return of a String that is similar version of the original character array that has been secured.
 	public static String secureItem(String item){
 
+				//Inititialization of: passphrase to be returned,,,index to keep track of substrting location,,,random number,,,random seed to ensure different number every time program is used
 				String passphrase = "";
 				int index = 0;
 				Random rand = new Random();
+				long seed = rand.nextLong();
+				rand.setSeed(seed);
+
+				//This loop iterates through the input string, changing each character to either the same character, a capatilized character, a similar number or similar symbol
 				for(int i = 0; i < item.length(); i++){
 					
 					String letter = item.substring(index, i+1);
